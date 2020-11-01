c1d911e60e11c7d1f9a75c79d434c4fd42f21ff2
021800527 <978621025@qq.com>
2020-11-01 22:30:50 +0800

    首页今日诗词

---
+++ pages/font_page/font_page.js
@@ -0,0 +1,70 @@
+// pages/font_page/font_page.js
+const jinrishici = require('../../utils/jinrishici.js')
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    jinrishici:'',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    jinrishici.load(result => {
+      // 下面是处理逻辑示例
+      console.log(result),
+      this.setData({"jinrishici": result.data.content})
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    wx.hideHomeButton();
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
